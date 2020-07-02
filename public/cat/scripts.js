5ab913ed891780c8adb78932d61fff0ad02b2f26
myriam mansouri <myriam.mansouri@gmail.com>
2020-07-02 14:45:03 -0400

    Exercise 1

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,35 @@
 // here there be JS, yarrr ☠️
+const messageInput = document.querySelector("#user-input");
+const conversationElem = document.querySelector("#conversation-container");
+
+const handleFocus = () => {
+  messageInput.focus();
+};
+
+const updateConversation = (message) => {
+  const { author, text } = message;
+  const messageElem = document.createElement("p");
+  messageElem.innerHTML = `<span>${text}</span>`;
+  conversationElem.appendChild(messageElem);
+  messageElem.classList.add("message", author);
+
+  if (author === "user") {
+    messageInput.value = "";
+  }
+
+  conversationElem.scrollTop = conversationElem.scrollHeight;
+};
+const sendMessage = (event) => {
+  event.preventDefault();
+  const message = { author: "user", text: messageInput.value };
+
+  updateConversation(message);
+
+  fetch("/cat-message")
+    .then((res) => res.json())
+    .then((data) => {
+      updateConversation(data.message);
+    });
+};
+
+handleFocus();
